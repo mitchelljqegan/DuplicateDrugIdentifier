930019a41bf309f66b42045ff323751169b6cb5a
Mitchell Egan <39621820+mitchelljqegan@users.noreply.github.com>
2021-09-12 12:58:40 +1000

    Merge and adapt Permutations to existing code

--- Program.cs
+++ Program.cs
@@ -2,59 +2,109 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using System.Linq;
 
 namespace DuplicateDrugIdentifier
 {
+    public class Entry
+    {
+        public string ItemCode { get; set; }
+        public string Description { get; set; }
+        public string PrimarySupplier { get; set; }
+        public string PrimarySupplierProductCode { get; set; }
+        public string Barcode { get; set; }
+        public string ProductGroup { get; set; }
+        public string ProductGroupName { get; set; }
+        public double? RetailPrice { get; set; }
+        public double QtyOnHand { get; set; }
+        public int QtyOnOrder { get; set; }
+        public DateTime? CreationDate { get; set; }
+        public string Status { get; set; }
+        public DateTime? LastSellPriceChange { get; set; }
+
+        public Entry(string[] values)
+        {
+
+            ItemCode = values[0];
+            Description = values[1];
+            PrimarySupplier = values[2];
+            PrimarySupplierProductCode = values[3];
+            Barcode = values[4];
+            ProductGroup = values[5];
+            ProductGroupName = values[6];
+
+            if (double.TryParse(values[7], out double retailPrice))
+            {
+                RetailPrice = retailPrice;
+            }
+            else
+            {
+                RetailPrice = null;
+            }
+
+            QtyOnHand = double.Parse(values[8]);
+            QtyOnOrder = int.Parse(values[9]);
+
+            if (DateTime.TryParse(values[10], out DateTime creationDate))
+            {
+                CreationDate = creationDate;
+            }
+            else
+            {
+                CreationDate = null;
+            }
+
+            Status = values[11];
+
+            if (DateTime.TryParse(values[12], out DateTime lastSellPriceChange))
+            {
+                LastSellPriceChange = lastSellPriceChange;
+            }
+            else
+            {
+                LastSellPriceChange = null;
+            }
+        }
+    }
+
     class Program
     {
-        enum Index : int
-        {
-            ItemCode = 0,
-            Description = 1,
-            PrimarySupplier = 2,
-            PrimarySupplierProductCode = 3,
-            Barcode = 4,
-            ProductGroup = 5,
-            ProductGroupName = 6,
-            RetailPrice = 7,
-            QtyOnHand = 8,
-            QtyOnOrder = 9,
-            CreationDate = 10,
-            Status = 11,
-            LastSellPriceChange = 12
-        }
+        const int apiqIndex = 0;
+        const int sigmaIndex = 1;
 
         static void Main()
         {
-            const char delimiter = '\t';
-
-            List<List<string>> entries;
-            List<string> Descriptions;
+            List<List<Entry>> entries;
             string filePath;
+            StringBuilder csv;
 
             filePath = GetFilePath();
+            entries = GetEntries(filePath);
+            csv = new StringBuilder();
 
-            entries = GetEntries(filePath, delimiter);
+            csv.AppendLine("SIGMA, APIQ, Similarity (Less = Closer)");
 
-            //Print all Descriptions
-            //Descriptions = GetDescriptions(entries);
-            //Descriptions.ForEach(Console.WriteLine);
-
-            List<int[]> indexes;
-            List<string[]> matches;
-            (indexes,matches) = FindPermutations(entries);
-            for (int i = 0; i < indexes.Count(); i++)
+            foreach (Entry sigmaEntry in entries[sigmaIndex])
             {
-                Console.WriteLine(indexes[i][0].ToString() + "," + matches[i][0] + "," + indexes[i][1].ToString() + "," + matches[i][1]);
+                int minDistance = int.MaxValue;
+                string closest = "";
+
+                foreach (Entry apiqEntry in entries[apiqIndex])
+                {
+                    int distance = LevenshteinDistance(sigmaEntry.Description, apiqEntry.Description);
+
+                    if (distance < minDistance)
+                    {
+                        minDistance = distance;
+                        closest = apiqEntry.Description;
+                    }
+                }
+
+                csv.AppendLine(string.Format("{0}, {1}, {2}", sigmaEntry.Description, closest, minDistance));
             }
 
-
-            Console.ReadLine();
-
-            // TODO - String matching (split description into name, quantity etc.)
-
-            // TODO - String matching (find drugs w/ same name & quantity)
+            File.WriteAllText("result.csv", csv.ToString());
         }
 
         private static string GetFilePath()
@@ -63,31 +113,77 @@ namespace DuplicateDrugIdentifier
             return Console.ReadLine().Replace("\"", string.Empty);
         }
 
-        private static List<List<string>> GetEntries(string filePath, char delimiter)
+        private static List<List<Entry>> GetEntries(string filePath)
         {
-            List<List<string>> entries;
+            List<List<Entry>> entries = new List<List<Entry>>();
 
-            entries = new List<List<string>>();
+            for (int i = 0; i < 2; i++)
+            {
+                entries.Add(new List<Entry>());
+            }
 
             using (StreamReader streamReader = new StreamReader(filePath))
             {
+                streamReader.ReadLine();
+
                 while (!streamReader.EndOfStream)
                 {
                     string line = streamReader.ReadLine();
-                    string[] values = line.Split(delimiter);
+                    string[] values = line.Split('\t');
 
                     for (int i = 0; i < values.Length; i++)
                     {
                         values[i] = values[i].Replace("\"", "");
                     }
 
-                    List<string> entry = new List<string>(values);
+                    Entry entry = new Entry(values);
 
-                    entries.Add(entry);
+                    if (entry.PrimarySupplier.Contains("APIQ"))
+                    {
+                        entries[apiqIndex].Add(entry);
+                    }
+                    else
+                    {
+                        entries[sigmaIndex].Add(entry);
+                    }
                 }
             }
 
             return entries;
+        }
+
+        static int LevenshteinDistance(string s, string t)
+        {
+            int n = s.Length;
+            int m = t.Length;
+            int[,] d = new int[n + 1, m + 1];
+
+            if (n == 0)
+            {
+                return m;
+            }
+
+            if (m == 0)
+            {
+                return n;
+            }
+
+            for (int i = 0; i <= n; i++)
+                d[i, 0] = i;
+            for (int j = 0; j <= m; j++)
+                d[0, j] = j;
+
+            for (int j = 1; j <= m; j++)
+                for (int i = 1; i <= n; i++)
+                    if (s[i - 1] == t[j - 1])
+                        d[i, j] = d[i - 1, j - 1];  //no operation
+                    else
+                        d[i, j] = Math.Min(Math.Min(
+                            d[i - 1, j] + 1,    //a deletion
+                            d[i, j - 1] + 1),   //an insertion
+                            d[i - 1, j - 1] + 1 //a substitution
+                            );
+            return d[n, m];
         }
 
         private static List<string> GetDescriptions(List<List<string>> entries)
